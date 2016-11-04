cc3e61d7e2e8254c984fcbffd70774a84c4a81d8
zhaixuezhong <zhaixuezhong@hotmail.com>
2016-11-04 10:48:44 +0800

    fix

--- src/design_pattern/singleton01/Car.java
+++ src/design_pattern/singleton01/Car.java
@@ -7,21 +7,36 @@ public class Car {
     /**
      * Created by zhaixuezhong on 11/4/16.
      */
-        private static Car instance = new Car();
-        int i = 0;
+    private volatile static Car instance;
+    int i = 1;
+    static int j = 0;
 
 
-        private Car(){}
+    private Car(){}
 
-        public static Car instanceCar() {
-            return instance;
+    public static Car instanceCar() {
+        if(instance == null){
+
+
+            initCar();
         }
 
+        return instance;
+    }
 
-        public void showMessage(){
-            System.out.println("Hello World! " + i + " times!");
-            i ++;
-            }
+
+
+    public static synchronized void initCar(){
+        instance = new Car();
+        j ++;
+    }
+
+
+    public void showMessage(){
+        System.out.println("Hello World! " + i + " times! and init ");
+        i ++;
+
+    }
 }
 
 class Test {
@@ -29,18 +44,17 @@ class Test {
     public static void main(String[] args) {
 
 
-        Car car = Car.instanceCar();
-
-
-        Car car2 = Car.instanceCar();
-
-        Car car3 = Car.instanceCar();
-
-
-        car.showMessage();
-        car2.showMessage();
-        car2.showMessage();
-        car3.showMessage();
+//        Car car = Car.instanceCar();
+//
+//        Car car2 = Car.instanceCar();
+//
+//        Car car3 = Car.instanceCar();
+//
+//
+//        car.showMessage();
+//        car2.showMessage();
+//        car2.showMessage();
+//        car3.showMessage();
 
         //every time we invoke Car.instanceCar() they just return the same object
 
